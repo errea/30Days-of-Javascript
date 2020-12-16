b2bd8a3df3ca598adf9cfc2fb718b5b127decd40
errea <erreakezy@gmail.com>
2020-12-16 14:22:14 +0100

    modified day_2-Operators.js

--- day_2-Operators.js
+++ day_2-Operators.js
@@ -1,5 +1,5 @@
 // Write your code here
-mealCost = Math.round(mealCost + (mealCost * tipPercent/100) + (mealCost * taxPercent/100));
-
+let total_cost;
+    total_cost =  Math.round(meal_cost + meal_cost * tip_percent/100 + meal_cost * tax_percent/100);
 // Use console.log() to print to stdout
-console.log('The total meal cost is ' + mealCost + ' dollars.');
+console.log(total_cost);
