dacad1f61310c36a1284c7e110706655316d5d6b
errea <erreakezy@gmail.com>
2020-12-25 00:05:34 +0100

    modified Day 9

--- day_9-Recursion.js
+++ day_9-Recursion.js
@@ -1,8 +1,18 @@
-function factorial(n) {
+/* function factorial(n) {
   // Complete this function
   var result = 1;
   for (i = n; i >= 1; i--) {
     result *= i
   }
   return result
-}
+}*/
+
+function factorial(n) {
+  if (n <= 1) {
+          return 1;
+      }
+      else {
+           return n * factorial(n - 1);
+      }
+      console.log(factorial(input));
+  }
